bddb0390b82e5ca47ab71b8480c67da02f5da066
Angie.Rochi <rocio.taiuti.09@gmail.com>
2024-11-05 18:04:40 -0300

    Tests

--- app/src/test/java/com/example/missingyou/ExampleUnitTest.kt
+++ app/src/test/java/com/example/missingyou/ExampleUnitTest.kt
@@ -1,17 +1,38 @@
-package com.example.missingyou
-
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.*
 
-import org.junit.Assert.*
+class MainActivityTest {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+    private lateinit var activity: MainActivity
+    private lateinit var sharedPreferences: SharedPreferences
+
+    @Before
+    fun setUp() {
+        activity = MainActivity()
+        sharedPreferences = ApplicationProvider.getApplicationContext<Context>()
+            .getSharedPreferences("CountPrefs", Context.MODE_PRIVATE)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun incrementCount_increasesCountByOne() {
+        val initialCount = activity.count
+        activity.incrementCount()
+        assertEquals(initialCount + 1, activity.count)
+    }
+
+    @Test
+    fun resetCountIfNewDay_resetsCountOnNewDay() {
+        // Configura la fecha de ayer en sharedPreferences
+        val editor = sharedPreferences.edit()
+        editor.putString("lastDate", "2024-11-04") // Suponiendo que hoy es "2024-11-05"
+        editor.apply()
+
+        activity.resetCountIfNewDay()
+        assertEquals(0, activity.count)
     }
 }
